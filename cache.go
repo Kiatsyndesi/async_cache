f06722235b946b64a9fdf8d9917531f91d0a4226
Кияница Денис <kiyanitsadenis@gmail.com>
2022-12-22 00:16:25 +0700

    first simple implementation

---
+++ cache.go
@@ -0,0 +1,43 @@
+package async_cache
+
+import "errors"
+
+var ErrNotFound = errors.New("Value not found")
+
+type CacheMethods interface {
+	Set(key, value string) error
+	Get(key string) (string, error)
+	Delete(key string) error
+}
+
+type Cache struct {
+	storage map[string]string
+}
+
+func (c *Cache) Set(key, value string) error {
+	c.storage[key] = value
+
+	_, err := c.Get(key)
+	if err != nil {
+		return ErrNotFound
+	}
+}
+
+func (c *Cache) Get(key string) (string, error) {
+	value, ok := c.storage[key]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return value, nil
+}
+
+func (c *Cache) Delete(key string) error {
+	delete(c.storage, key)
+
+	_, err := c.Get(key)
+	if err == nil {
+		return errors.New("the key has not been removed\n")
+	}
+}
