2510a1b3ee782d8aeedfb335a8f80862775ad059
Кияница Денис <kiyanitsadenis@gmail.com>
2023-01-25 23:14:04 +0700

    add cache variant with mutexes and test for it

---
+++ cache_v2/cache_v2_test.go
@@ -0,0 +1,60 @@
+package cache_v2
+
+import (
+	"github.com/Kiatsyndesi/async_cache/cache_test_helpers"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCacheV2(t *testing.T) {
+	t.Parallel()
+
+	testCache := NewCache()
+
+	t.Run("Check stored value", func(t *testing.T) {
+		t.Parallel()
+
+		key := "Avenger"
+		value := "Iron Man"
+
+		err := testCache.Set(key, value)
+		assert.NoError(t, err)
+
+		storedValue, err := testCache.Get(key)
+		assert.NoError(t, err)
+
+		assert.EqualValues(t, value, storedValue)
+	})
+
+	t.Run("Check update of existing key", func(t *testing.T) {
+		t.Parallel()
+
+		key := "Avenger"
+		value := "Iron Man"
+		newValue := "Thor"
+
+		err := testCache.Set(key, value)
+		assert.NoError(t, err)
+
+		storedValue, err := testCache.Get(key)
+		assert.NoError(t, err)
+
+		assert.EqualValues(t, value, storedValue)
+
+		err = testCache.Set(key, newValue)
+		assert.NoError(t, err)
+
+		storedValue, err = testCache.Get(key)
+		assert.NoError(t, err)
+
+		assert.EqualValues(t, newValue, storedValue)
+	})
+
+	t.Run("Check for data races", func(t *testing.T) {
+		t.Parallel()
+
+		parallelFactor := 100_000
+
+		cache_test_helpers.EmulateLoad(t, testCache, parallelFactor)
+	})
+}
